695099dd35cca236427ba4c2f5aa16d42d1dcf34
yuriy <yuriy.turetskiy@gmail.com>
2018-06-05 20:07:36 -0400

    add views for page

--- app/admin/page.rb
+++ app/admin/page.rb
@@ -1,8 +1,8 @@
-ActiveAdmin.register Pages do
+ActiveAdmin.register Page do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-# permit_params :list, :of, :attributes, :on, :model
+permit_params :title, :image_url, :body
 #
 # or
 #
